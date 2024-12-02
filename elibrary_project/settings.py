b9385a48f6a51906979d988f56d2af3ddeaeb958
Akash Pal <91068643+akashjpal@users.noreply.github.com>
2024-12-02 15:05:33 +0530

    Update settings.py

--- elibrary_project/settings.py
+++ elibrary_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&rp=ja%cbum9eq2vl1f$6+pkl6^2g*k7qv)zz24q4_k19%t7*a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
